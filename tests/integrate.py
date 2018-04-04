ae34101ae7f5316155b37c41fba65125e2a633ea
Ning <hueyning@minerva.kgi.edu>
2018-04-04 10:35:00 +0530

    fixed failure in integrate.py

--- tests/integrate.py
+++ tests/integrate.py
@@ -6,7 +6,7 @@ class TestUM(unittest.TestCase):
         pass
  
     def test_numbers_3_4(self):
-        self.assertEqual( 3**4, 12)
+        self.assertEqual( 3*4, 12)
  
     def test_strings_a_3(self):
         self.assertEqual( 'a'*3, 'aaa')
